9c3b17369606e82af4cd27cb1b80a0bfd0cc9c97
Dan Tutton <dtutton@msn.com>
2017-06-22 12:53:42 -0700

    animation, stop anim button working

--- src/app/animation-test/animation-test.component.ts
+++ src/app/animation-test/animation-test.component.ts
@@ -11,12 +11,22 @@ export class AnimationTestComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    var interval = setInterval(function(){ 
+      var logoLg = document.getElementById("opening-logo-lg");
+      logoLg.classList.remove("background-transition");
+      logoLg.classList.add("logo-closing-transition");
+      var logoMd = document.getElementById("opening-logo-md");
+      logoMd.classList.remove("background-transition");
+      logoMd.classList.add("logo-closing-transition");
+      var logoXs = document.getElementById("opening-logo-xs");
+      logoXs.classList.remove("background-transition");
+      logoXs.classList.add("logo-closing-transition");
+      clearInterval(interval);
+    }, 1000);
   }
 
-  skipAnimation() {
-    // alert("skip pressed")
+  public skipAnimation() {
     this.onSkipAnimation.emit();
   }
-
-
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +8,52 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 
+  ngOnInit() {
+    // alert("onInit");
+    var animationInterval;
+    animationInterval = setInterval(function(){ 
+      var website = document.getElementById("mainWebsite");
+      website.classList.remove("website");
+      website.classList.remove("website-transition");
+      website.classList.add("website-no-opening");
+
+      var logosmall = document.getElementById("logo-small");
+      logosmall.classList.remove("logo-small-top-adjust");
+
+      var parent = document.getElementById("animationContainer");
+      var child = document.getElementById("openingAnimation");
+      parent.removeChild(child);
+      clearInterval(animationInterval);
+    }, 23000);
+  }
+
   public onSkipAnimation() {
     // alert("Event Emitted");
     var website = document.getElementById("mainWebsite");
     website.classList.remove("website");
     website.classList.remove("website-transition");
+    website.classList.add("website-no-opening");
+
+    var logosmall = document.getElementById("logo-small");
+    logosmall.classList.remove("logo-small-top-adjust");
+
     var parent = document.getElementById("animationContainer");
     var child = document.getElementById("openingAnimation");
     parent.removeChild(child);
+  }
+
+  goToWebsite() {
+      var website = document.getElementById("mainWebsite");
+      website.classList.remove("website");
+      website.classList.remove("website-transition");
+      website.classList.add("website-no-opening");
+
+      var logosmall = document.getElementById("logo-small");
+      logosmall.classList.remove("logo-small-top-adjust");
+
+      var parent = document.getElementById("animationContainer");
+      var child = document.getElementById("openingAnimation");
+      parent.removeChild(child);
   }
 
 }
